4289a615cc743da6bdf82cebc1fb04463c5af6b6
sijabatwahyujudika <sijabatwahyujudika@gmail.com>
2026-07-08 02:38:40 +0700

    Use PHP 8.4

--- tests/Feature/AuthLoginTest.php
+++ tests/Feature/AuthLoginTest.php
@@ -1,16 +1,23 @@
 <?php
 
+namespace Tests\Feature;
+
 use App\Models\User;
 use Database\Seeders\UserSeeder;
+use Tests\TestCase;
 
-it('allows login with seeded user credentials', function () {
-    $this->seed(UserSeeder::class);
+class AuthLoginTest extends TestCase
+{
+    public function testAllowsLoginWithSeededUserCredentials(): void
+    {
+        $this->seed(UserSeeder::class);
 
-    $response = $this->post('/login', [
-        'email' => 'anggota@kampus.ac.id',
-        'password' => 'anggota123',
-    ]);
+        $response = $this->post('/login', [
+            'email' => 'anggota@kampus.ac.id',
+            'password' => 'anggota123',
+        ]);
 
-    $response->assertRedirect('/produk');
-    $this->assertAuthenticatedAs(User::where('email', 'anggota@kampus.ac.id')->first());
-});
+        $response->assertRedirect('/produk');
+        $this->assertAuthenticatedAs(User::where('email', 'anggota@kampus.ac.id')->first());
+    }
+}
